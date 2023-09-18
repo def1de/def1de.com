17473209efa46daa646c075585e06b1b6100386c
Illia Katerynych <iliakat.kr@gmail.com>
2023-09-18 21:19:37 +0100

    main page remake

--- js/script.js
+++ js/script.js
@@ -1,78 +1,46 @@
-const track = document.getElementById("nav-track");
+/* -- Glow effect -- */
 
-const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+const blob = document.getElementById("blob");
 
-const handleOnUp = () => {
-    track.dataset.mouseDownAt = "0";
-    track.dataset.prevPercentage = track.dataset.percentage;
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
 }
 
-const handleOnMove = e => {
-    if (track.dataset.mouseDownAt === "0") return;
+/* -- Text effect -- */
 
-    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
-        maxDelta = window.innerWidth / 2;
-
-    const percentage = (mouseDelta / maxDelta) * -100,
-        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
-        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -50);
-
-    track.dataset.percentage = nextPercentage;
-
-    track.animate({
-        transform: `translate(${nextPercentage}%, -50%)`
-    }, { duration: 1200, fill: "forwards" });
-
-    for (const image of track.getElementsByClassName("destination")) {
-        image.animate({
-            backgroundPosition: `${100 + nextPercentage}% center`
-        }, { duration: 1200, fill: "forwards" });
-    }
-}
-
-
-const nickname = document.getElementById("nickname")
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let interval = null;
 
-function animateNickname() {
-    let iteration = 0;
+const screen = document.querySelector(".screen"),
+      name = document.querySelector(".name");
 
-    clearInterval(interval);
-
-    interval = setInterval(() => {
-        nickname.innerText = nickname.innerText
-            .split("")
-            .map((letter, index) => {
-                if (index < iteration) {
-                    return nickname.dataset.value[index];
-                }
-
-                return letters[Math.floor(Math.random() * 26)]
-            })
-            .join("");
-
-        if (iteration >= nickname.dataset.value.length) {
-            clearInterval(interval);
+screen.onmouseenter = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    name.innerText = name.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return name.dataset.value[index];
         }
-
-        iteration += 1 / 3;
-    }, 30);
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= name.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
 }
-
-
-/* -- lines for touch events -- */
-setInterval(animateNickname(), 1000);
-
-window.onmousedown = e => handleOnDown(e);
-
-window.ontouchstart = e => handleOnDown(e.touches[0]);
-
-window.onmouseup = e => handleOnUp(e);
-
-window.ontouchend = e => handleOnUp(e.touches[0]);
-
-window.onmousemove = e => handleOnMove(e);
-
-window.ontouchmove = e => handleOnMove(e.touches[0]); s
