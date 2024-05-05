3a10154ff662134cc51a0cd95b68c5a40f64c644
Illia Katerynych <iliakat.kr@gmail.com>
2024-05-05 12:01:46 +0100

    About section added

--- js/script.js
+++ js/script.js
@@ -4,11 +4,32 @@ const aboutContent = document.querySelector('#about-me');
 const projects = document.querySelector('#projects');
 const projectsContent = document.querySelector('#my-projects');
 
+const progressBar = document.getElementsByClassName('progress-bar');
+
+window.onload = () => {
+    loadProgressBar();
+}
+
+loadProgressBar = () => {
+    for (let i = 0; i < progressBar.length; i++) {
+        progressBar[i].classList.add('active');
+    }
+};
+
+unloadProgressBar = () => {
+    for (let i = 0; i < progressBar.length; i++) {
+        progressBar[i].classList.remove('active');
+    }
+};
+
 about.addEventListener('click', () => {
     about.classList.add('active');
     projects.classList.remove('active');
     aboutContent.style.transform = 'translateX(-50%)';
     projectsContent.style.transform = 'translateX(100%)';
+    setTimeout(() => {
+        loadProgressBar();
+    }, 100);
 });
 
 projects.addEventListener('click', () => {
@@ -16,4 +37,7 @@ projects.addEventListener('click', () => {
     about.classList.remove('active');
     projectsContent.style.transform = 'translateX(-50%)';
     aboutContent.style.transform = 'translateX(100%)';
+    setTimeout(() => {
+        unloadProgressBar();
+    }, 500);
 });
